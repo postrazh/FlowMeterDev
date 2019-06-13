73f4847251405aa9d5d7be1696536480083f8c79
valor <valor312@gmail.com>
2019-06-14 00:55:40 +0800

    recover the diversed code

--- FlowMeter/FlowMeter/View/MainWindow.xaml.cs
+++ FlowMeter/FlowMeter/View/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FlowMeter
 {
@@ -26,13 +27,30 @@ namespace FlowMeter
     /// </summary>
     public partial class MainWindow : Window
     {
-        ALDSerialPort serial;
+        private ALDSerialPort serial;
+
+        private string strBuffer = "";
+
+        private enum CalcMode
+        {
+            NONE,
+            STARTED_EXTERNAL_VOLUME,
+            STARTED_FLOW_RATE
+        }
+        private CalcMode calcMode = CalcMode.NONE;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            // initialize values of controls
             lblPortName.Content = SettingsManager.getInstance().CurrentSettings.SerialConfig.PortName;
+
+            // start the timer
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += TimerTick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
 
             var myMessageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(1000));
             MySnackbar.MessageQueue = myMessageQueue;
@@ -89,18 +107,89 @@ namespace FlowMeter
 
         private void serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
+            // accumulate the recevied string
             string strReceived = serial.Serial.ReadExisting();
+            string strFull = "";
+            if (strReceived.Contains('\r'))
+            {
+                strFull = strBuffer + strReceived;
+                strBuffer = "";
+            }
+            else
+            {
+                strBuffer += strReceived;
+                return;
+            }
+
+            // log to rich textbox
+            this.Dispatcher.Invoke(new Action(delegate
+            {
+                this.richTxtConsole.AppendText(strFull);
+                this.richTxtConsole.ScrollToEnd();
+            }));
+
+
+            // extract the command
+            string command = strFull.Substring(0, 3);
+            string strValue = strFull.Substring(3).TrimStart('+').TrimEnd('\r', '\n');
 
             this.Dispatcher.Invoke(new Action(delegate
             {
-                if (serial.LastSent.Substring(0, 3) == "@10")
+                switch (command)
                 {
-                    txtMaxPressure.Text = strReceived.Substring(3);
+                    case "@10":
+                        txtMaxPressure.Text = strValue;
+                        break;
+                    case "@11":
+                        txtTimeout.Text = strValue;
+                        break;
+                    case "@14":
+                        txtMinPressure.Text = strValue;
+                        break;
+                    case "@17":
+                        txtPurgeCycles.Text = strValue;
+                        break;
+                    case "@18":
+                        txtStabilizationTime.Text = strValue;
+                        break;
+                    case "@40":
+                        txtBasePressure.Text = strValue;
+                        break;
+                    case "@16":
+                        txtStrayTotal.Text = strValue;
+                        break;
+                    // calculate external volume
+                    case "@01":
+                        startedExternalVolume();
+                        break;
+                    case "=01":
+                        failedToStartExternalVolume();
+                        break;
+                    case "@20":
+                        if (strValue == "0A")
+                            readyExternalVolume();
+                        else if (strValue == "00")
+                            completedFlowRate();
+                        break;
+                    case "@04":
+                        continueExternalVolume();
+                        break;
+                    case "@23":
+                        reportExternalVolume(strValue);
+                        break;
+                    // calculate flow rate
+                    case "@00":
+                        startedFlowRate();
+                        break;
+                    case "=00":
+                        failedToStartFlowRate();
+                        break;
+                    case "@21":
+                        lblFlowValue.Content = strValue;
+                        break;
                 }
+            }));
 
-                this.richTxtConsole.AppendText(strReceived); // "\n" + 
-                this.richTxtConsole.ScrollToEnd();
-            }));            
         }
 
         private void TglConnect_Unchecked(object sender, RoutedEventArgs e)
@@ -197,54 +286,34 @@ namespace FlowMeter
             SumupStrayVolume();
         }
 
-        private void BtnCalculateExternal_Click(object sender, RoutedEventArgs e)
-        {
-            PasswordInput passwordInput = new PasswordInput();
-            passwordInput.Owner = this;
-            if (passwordInput.ShowDialog() == true)
-            {
-                Debug.WriteLine("Changed the serial port configuration.");
-            }
-        }
-
-        private void BtnCalculateFlow_Click(object sender, RoutedEventArgs e)
-        {
-            if (serial == null || serial.Serial.IsOpen == false)
-            {
-                return;
-            }
-
-            sendToSerial("Hello\r\n");
-            sendToSerial("@01?\r\n");
-        }
-
-        private void sendToSerial(string strData)
+        private bool sendToSerial(string strData)
         {
             if (serial == null || serial.Serial.IsOpen == false)
             {
                 showSnackBarMessage("Serial Port is not opened!");
-                return;
+                return false;
             }
             if (!serial.SendData(strData))
             {
                 showSnackBarMessage("Can not send to the Serial Port!");
-                return;
+                return false;
             }
 
             string append;
             append = ">> ";
             append += strData;
             richTxtConsole.AppendText(append);
+			return true;
         }
 
         private void BtnReadStray_Click(object sender, RoutedEventArgs e)
         {
-
+            sendToSerial("@16?\r\n");
         }
 
         private void BtnWriteStray_Click(object sender, RoutedEventArgs e)
         {
-
+            sendToSerial("@16" + ToOneDecimal(txtStrayTotal.Text) + "\r\n");
         }
 
         private void BtnReadMaxPressure_Click(object sender, RoutedEventArgs e)
@@ -264,7 +333,7 @@ namespace FlowMeter
 
         private void BtnWriteTimeout_Click(object sender, RoutedEventArgs e)
         {
-
+            sendToSerial("@11" + txtTimeout.Text + "\r\n");
         }
 
         private void BtnReadMinPressure_Click(object sender, RoutedEventArgs e)
@@ -274,7 +343,7 @@ namespace FlowMeter
 
         private void BtnWriteMinPressure_Click(object sender, RoutedEventArgs e)
         {
-
+            sendToSerial("@14" + txtMinPressure.Text + "\r\n");
         }
 
         private void BtnReadPurgeCycles_Click(object sender, RoutedEventArgs e)
@@ -284,7 +353,7 @@ namespace FlowMeter
 
         private void BtnWritePurgeCycles_Click(object sender, RoutedEventArgs e)
         {
-
+            sendToSerial("@17" + txtPurgeCycles.Text + "\r\n");
         }
 
         private void BtnReadStabilizationTime_Click(object sender, RoutedEventArgs e)
@@ -294,7 +363,7 @@ namespace FlowMeter
 
         private void BtnWriteStabilizationTime_Click(object sender, RoutedEventArgs e)
         {
-
+            sendToSerial("@18" + txtStabilizationTime.Text + "\r\n");
         }
 
         private void BtnReadBasePressure_Click(object sender, RoutedEventArgs e)
@@ -304,7 +373,15 @@ namespace FlowMeter
 
         private void BtnWriteBasePressure_Click(object sender, RoutedEventArgs e)
         {
+            sendToSerial("@40" + txtBasePressure.Text + "\r\n");
+        }
 
+        private string ToOneDecimal(string strValue)
+        {
+            double value = Convert.ToDouble(strValue);
+            value = Math.Round(value, 1);
+            string retVal = value.ToString("0.0");
+            return retVal;
         }
 
         private void TglP5a_Checked(object sender, RoutedEventArgs e)
@@ -355,6 +432,159 @@ namespace FlowMeter
         private void Toggle_Changed(object sender, RoutedEventArgs e)
         {
             SumupStrayVolume();
+        }
+		private void BtnCalculateFlow_Click(object sender, RoutedEventArgs e)
+        {
+            sendToSerial("@40?\r\n");
+        }
+
+        
+
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (calcMode == CalcMode.STARTED_EXTERNAL_VOLUME)
+            {
+                sendToSerial("@20?\r\n");
+            }
+            else if (calcMode == CalcMode.STARTED_FLOW_RATE)
+            {
+                sendToSerial("@20?\r\n");
+            }
+        }
+
+        private void BtnExternalStart_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordInput passwordInput = new PasswordInput();
+            passwordInput.Owner = this;
+            if (passwordInput.ShowDialog() != true)
+            {
+                return;
+            }
+
+            if (calcMode == CalcMode.NONE)
+            {
+                if (!sendToSerial("@01\r\n"))
+                    return;
+
+                lblExternalStatus.Content = "Starting...";
+                progressExternal.Visibility = Visibility.Visible;
+                btnExternalStart.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Can not start!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void startedExternalVolume()
+        {
+            calcMode = CalcMode.STARTED_EXTERNAL_VOLUME;
+            lblExternalStatus.Content = "Monitoring...";
+            progressExternal.Visibility = Visibility.Visible;
+        }
+
+        private void failedToStartExternalVolume()
+        {
+            lblExternalStatus.Content = "---";
+            progressExternal.Visibility = Visibility.Hidden;
+            btnExternalStart.IsEnabled = true;
+
+            MessageBox.Show("Command is inappropriate.\n" +
+                            "The GBR3B is currently performing another operation.",
+                            "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void readyExternalVolume()
+        {
+            calcMode = CalcMode.NONE;
+            lblExternalStatus.Content = "Waiting to stop";
+            btnExternalStop.Visibility = Visibility.Visible;
+            progressExternal.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnExternalStop_Click(object sender, RoutedEventArgs e)
+        {
+            lblExternalStatus.Content = "Continue";
+            btnExternalStop.Visibility = Visibility.Collapsed;
+            btnExternalStart.IsEnabled = true;
+
+            sendToSerial("@04\r\n");
+        }
+
+        private void continueExternalVolume()
+        {
+            sendToSerial("@23?\r\n");
+        }
+
+        private void reportExternalVolume(string strValue)
+        {
+            double value = Convert.ToDouble(strValue);
+            if (value < 0)
+            {
+                lblExternalStatus.Content = "Fail";
+                lblExternalValue.Content = "---";
+            }
+            else
+            {
+                lblExternalStatus.Content = "Success";
+                lblExternalValue.Content = strValue;
+            }
+        }
+
+        private void BtnFlowStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (calcMode == CalcMode.NONE)
+            {
+                if (!sendToSerial("@00\r\n"))
+                    return;
+
+                lblFlowStatus.Content = "Starting...";
+                progressFlow.Visibility = Visibility.Visible;
+                btnFlowStart.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Can not start!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void startedFlowRate()
+        {
+            calcMode = CalcMode.STARTED_FLOW_RATE;
+            lblFlowStatus.Content = "Monitoring...";
+            progressFlow.Visibility = Visibility.Visible;
+        }
+
+        private void completedFlowRate()
+        {
+            calcMode = CalcMode.NONE;
+            lblFlowStatus.Content = "Success";
+            progressFlow.Visibility = Visibility.Hidden;
+            btnFlowStart.IsEnabled = true;
+
+            sendToSerial("@21?\r\n");                       // ask the flow rate
+        }
+
+        private void failedToStartFlowRate()
+        {
+            lblFlowStatus.Content = "---";
+            progressFlow.Visibility = Visibility.Hidden;
+            btnFlowStart.IsEnabled = true;
+
+            MessageBox.Show("Command is inappropriate.\n" +
+                            "The GBR3B is currently performing another operation or the external volume has not been calculated yet.",
+                            "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void BtnContinueGbror_Click(object sender, RoutedEventArgs e)
+        {
+            sendToSerial("@04\r\n");
+        }
+
+        private void BtnAbortGbror_Click(object sender, RoutedEventArgs e)
+        {
+            sendToSerial("@05\r\n");
         }
     }
 }
